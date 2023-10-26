e86c020d5a0c11c0227b38e3a5b26709d477f882
mo-zza <bigsmalljoe@gmail.com>
2023-10-26 14:36:35 +0900

    test: auth servie application

--- auth-service/src/test/java/com/muscatlab/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/muscatlab/authservice/AuthServiceApplicationTests.java
@@ -1,13 +1,14 @@
 package com.muscatlab.authservice;
 
+import com.muscatlab.authservice.config.TestConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@Import({TestConfig.class})
 class AuthServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
