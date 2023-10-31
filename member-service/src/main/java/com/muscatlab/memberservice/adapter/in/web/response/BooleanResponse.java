7a13f31ae411622389b2851bcfbc5fb0fa16d62e
mo-zza <bigsmalljoe@gmail.com>
2023-10-31 10:11:51 +0900

    feat: implement boolean response dto

---
+++ member-service/src/main/java/com/muscatlab/memberservice/adapter/in/web/response/BooleanResponse.java
@@ -0,0 +1,12 @@
+package com.muscatlab.memberservice.adapter.in.web.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record BooleanResponse(
+        boolean result
+) {
+}
